cf26b66cc930a7d696296fd9907e426ec675fb22
unknown <kchanguin@gmail.com>
2023-03-19 00:44:27 -0400

    new attempt to meet the criteria

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # module_2_challenge
+cloned SSH key from Xandorus user to attempt in matching the criteria.
--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 // Assignment code here
-
-
 // Get references to the #generate element
+//help setup user for the password criteria
+    //password length
+    //lowercase, uppercase, and include special characters
+//approve input (make sure password length is between 8-128 characters
+//generate the password
+//present password on page (strong password and provides greater security)
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){
+const characters =
+"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%^&*()_+?<>:{}[]"
+
+var passwordLength = 16;
+var password = '';
+
+for (var i = 0; i <passwordLength; i++) {
+var randomNumber = Math.floor(Math.random ()
+* characters.length);  
+
+password += characters.substring(randomNumber, randomNumber + 1);
+console.log(password);
+
+}
+document.getElementById("password").value = password;
+
+}
 
 // Write password to the #password input
 function writePassword() {
